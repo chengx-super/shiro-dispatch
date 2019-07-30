9c2769479de18b83a2fc83d6c732be910c1ef867
chenguangxu <guangxuchen@oneops.cn>
2019-07-30 17:59:34 +0800

    添加 alibaba druid 作为数据源 统一请求/响应验证

--- src/main/java/com/hengxc/shiro/ShiroApplication.java
+++ src/main/java/com/hengxc/shiro/ShiroApplication.java
@@ -1,15 +1,17 @@
 package com.hengxc.shiro;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
 @EnableAsync
 @EnableTransactionManagement
 @MapperScan("com.hengxc.shiro.*.mapper")
+
 public class ShiroApplication {
 
     public static void main(String[] args) {
